ca39069cf4101b3faf45ddef96f90c64bb67662e
Max Ivanov <waxivanov@gmail.com>
2023-07-27 14:51:56 +0300

    fix: add priority to image

--- src/components/uniswapLogo/index.tsx
+++ src/components/uniswapLogo/index.tsx
@@ -2,6 +2,12 @@ import Image from 'next/image';
 
 export const UniswapLogo: React.FC = () => {
   return (
-    <Image src="./uniswap-logo.svg" alt="uniswap-logo" width={64} height={64} />
+    <Image
+      src="./uniswap-logo.svg"
+      alt="uniswap-logo"
+      width={64}
+      height={64}
+      priority
+    />
   );
 };
