6d250698237e14264d4fa9f936433d179713b4e9
Max Ivanov <waxivanov@gmail.com>
2023-07-27 13:55:51 +0300

    init

---
+++ src/pages/_app.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { JSX } from 'react';
+import { AppProps } from 'next/app';
+import '../styles/global.css';
+
+const App = ({ Component, pageProps }: AppProps): JSX.Element => {
+  return <Component {...pageProps} />;
+};
+
+export default App;
